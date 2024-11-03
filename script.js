eef6ebe5a5ae24f20e931bbbcfdcd3e1ee376e3b
Moses <armildezmoses11olpstem@gmail.com>
2024-11-03 19:01:15 +0800

    Initial Commit

--- script.js
+++ script.js
@@ -10,5 +10,14 @@ function closePopup(className) {
 
 
 
-
-
+function updateTime() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  document.querySelector(".hour").innerHTML = `${hours}:`;
+  document.querySelector(".minute").innerHTML = minutes;
+}
+// Call updateTime every second
+setInterval(updateTime, 1000);
+// Initialize time on page load
+updateTime();
